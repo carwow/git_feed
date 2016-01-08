8b209ba75725ef98d093cf7d314a189009386a7f
Federico Toscano <federico.toscano@carwow.co.uk>
2016-01-08 18:11:13 +0000

    showing repo colors

--- client/git_feed.js
+++ client/git_feed.js
@@ -1,6 +1,3 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
 Template.commitList.onCreated(() => {
   Meteor.subscribe("commits");
 })
@@ -32,5 +29,33 @@ Template.commitCard.helpers({
     repo = repo.replace(/^(.)/g, function($1) { return $1.toUpperCase() })
     repo = repo.replace(/[-_]/g," ")
     return repo
+  },
+  showRepoTag(repo) {
+    return repo[0].toUpperCase()
+  },
+
+  strToColor(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let hex = ((hash>>24)&0xFF).toString(16) +
+            ((hash>>16)&0xFF).toString(16) +
+            ((hash>>8)&0xFF).toString(16) +
+            (hash&0xFF).toString(16);
+    // Sometimes the string returned will be too short so we
+    // add zeros to pad it out, which later get removed if
+    // the length is greater than six.
+    hex += '000000';
+    hex = '#'+hex.substring(0, 6);
+    console.log(hex)
+    hsv = tinycolor(hex).toHsv();
+    hsv.h /= 2;
+    hsv.s /= 2;
+    hex = tinycolor(hsv).toHexString();
+    console.log(hex)
+    return hex
   }
+
 });
