e70afc880842765a627d10099e18e3d4e5ec59ae
Federico Toscano <federico.toscano@carwow.co.uk>
2016-01-08 16:05:26 +0000

    showing basic commit cards

--- client/git_feed.js
+++ client/git_feed.js
@@ -7,7 +7,7 @@ Template.commitList.onCreated(() => {
 
 Template.commitList.helpers({
   commits() {
-    return Commits.find();
+    return Commits.find({});
   }
 });
 
@@ -15,5 +15,14 @@ Template.commitCard.helpers({
   escapeMessage(message) {
     let new_message = message.replace(/\n/g,'<br/>');
     return new_message
+  },
+  showAvatar(avatar_url) {
+    if (avatar_url) {
+      return avatar_url
+    }
+    return 'images/avatar.png'
+  },
+  showDate(date) {
+    return moment(date).format("DD/MM/YYYY HH:MM")
   }
 });
