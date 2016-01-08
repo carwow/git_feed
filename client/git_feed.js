002367bbdea8127bb97d24bf10390ee6a7ac9726
Ken Alex Fassone <nexusventuri@gmail.com>
2016-01-08 11:59:38 +0000

    Adding structure

---
+++ client/git_feed.js
@@ -0,0 +1,15 @@
+// counter starts at 0
+Session.setDefault('counter', 0);
+
+Template.hello.helpers({
+  counter() {
+    return Session.get('counter');
+  }
+});
+
+Template.hello.events({
+  'click button'() {
+    // increment the counter when button is clicked
+    Session.set('counter', Session.get('counter') + 1);
+  }
+});
